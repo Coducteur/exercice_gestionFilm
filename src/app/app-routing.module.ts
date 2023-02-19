d686f8e4e12b02096ec793780ed35cf9308226b2
Coducteur <chat.adr01@gmail.com>
2023-02-19 01:09:58 +0100

    logo + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavorisComponent } from './pages/favoris/favoris.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'movies', component: MoviesComponent },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+  },
+  {
+    path: 'favorite',
+    component: FavorisComponent,
+  },
 ];
 
 @NgModule({
